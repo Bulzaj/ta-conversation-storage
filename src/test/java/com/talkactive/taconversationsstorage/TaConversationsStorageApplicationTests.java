dbc64485ecd165b236a2cd5c2010865672983371
bulzaj <ernestsado94@gmail.com>
2020-09-20 16:51:27 +0200

    first commit

---
+++ src/test/java/com/talkactive/taconversationsstorage/TaConversationsStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkactive.taconversationsstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaConversationsStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
